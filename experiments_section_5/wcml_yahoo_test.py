52a8ca52ca51fb6dd03bbf29c9bfff538e6ebed0
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-10-01 10:03:40 -0400

    add wcml-yahoo-test

---
+++ experiments_section_5/wcml_yahoo_test.py
@@ -0,0 +1,123 @@
+import os
+import pickle
+import numpy as np
+from openrec.legacy import ImplicitModelTrainer
+from openrec.legacy.utils import ImplicitDataset
+from openrec.legacy.recommenders import WCML
+from openrec.legacy.utils.evaluators import AUC
+from openrec.legacy.utils.samplers import NPairwiseSampler
+from openrec.legacy.utils.evaluators import ImplicitEvalManager
+
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/dataset/yahoo/training_arr.npy")
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/dataset/yahoo/test_arr_pos.npy")
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/dataset/yahoo/test_arr_neg.npy")
+
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/best-models/wcml-yahoo/wcml-yahoo.data-00000-of-00001")
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/best-models/wcml-yahoo/wcml-yahoo.meta")
+os.system("wget https://s3.amazonaws.com/cornell-tech-sdl-rec-bias/best-models/wcml-yahoo/wcml-yahoo.index")
+
+raw_data = dict()
+raw_data['train_data'] = np.load("training_arr.npy")
+raw_data['test_data_pos'] = np.load("test_arr_pos.npy")
+raw_data['test_data_neg'] = np.load("test_arr_neg.npy")
+raw_data['max_user'] = 15401
+raw_data['max_item'] = 1001
+batch_size = 8000
+test_batch_size = 1000
+display_itr = 1000
+
+train_dataset = ImplicitDataset(raw_data['train_data'], raw_data['max_user'], raw_data['max_item'], name='Train')
+test_dataset_pos = ImplicitDataset(raw_data['test_data_pos'], raw_data['max_user'], raw_data['max_item'])
+test_dataset_neg = ImplicitDataset(raw_data['test_data_neg'], raw_data['max_user'], raw_data['max_item'])
+
+wcml_model = WCML(batch_size=batch_size, max_user=train_dataset.max_user(), max_item=train_dataset.max_item(), 
+    dim_embed=50, neg_num=5, l2_reg=0.001, opt='Adam', sess_config=None)
+sampler = NPairwiseSampler(batch_size=batch_size, dataset=train_dataset, negativenum=5, num_process=4)
+model_trainer = ImplicitModelTrainer(batch_size=batch_size, test_batch_size=test_batch_size,
+                                     train_dataset=train_dataset, model=wcml_model, sampler=sampler,
+                                     eval_save_prefix="wcml-yahoo",
+                                     item_serving_size=500)
+auc_evaluator = AUC()
+
+wcml_model.load("wcml-yahoo")
+
+model_trainer._eval_manager = ImplicitEvalManager(evaluators=[auc_evaluator])
+model_trainer._num_negatives = 300
+model_trainer._exclude_positives([train_dataset, test_dataset_pos, test_dataset_neg])
+model_trainer._sample_negatives(seed=10)
+
+model_trainer._eval_save_prefix = "wcml-yahoo-test-pos"
+model_trainer._evaluate_partial(test_dataset_pos)
+
+model_trainer._eval_save_prefix = "wcml-yahoo-test-neg"
+model_trainer._evaluate_partial(test_dataset_neg)
+
+
+def eq(infilename, infilename_neg, trainfilename, gamma=-1.0):
+    infile = open(infilename, 'rb')
+    infile_neg = open(infilename_neg, 'rb')
+    P = pickle.load(infile)
+    infile.close()
+    P_neg = pickle.load(infile_neg)
+    infile_neg.close()
+    NUM_NEGATIVES = P["num_negatives"]
+    #
+    for theuser in P["users"]:
+        neg_items = list(P_neg["user_items"][theuser][NUM_NEGATIVES:])
+        neg_scores = list(P_neg["results"][theuser][NUM_NEGATIVES:])
+        P["user_items"][theuser] = list(neg_items) + list(P["user_items"][theuser][NUM_NEGATIVES:])
+        P["results"][theuser] = list(neg_scores) + list(P["results"][theuser][NUM_NEGATIVES:])
+    #
+    Zui = dict()
+    Ni = dict()
+    # fill in dictionary Ni
+    trainset = np.load(trainfilename)
+    for i in trainset['item_id']:
+        if i in Ni:
+            Ni[i] += 1
+        else:
+            Ni[i] = 1
+    del trainset
+    # count #users with non-zero item frequencies
+    nonzero_user_count = 0
+    for theuser in P["users"]:
+        pos_items = P["user_items"][theuser][len(P_neg["user_items"][theuser][NUM_NEGATIVES:]):]
+        for pos_item in pos_items:
+            if pos_item in Ni:
+                nonzero_user_count += 1
+                break
+    # fill in dictionary Zui
+    for theuser in P["users"]:
+        all_scores = np.array(P["results"][theuser])
+        pos_items = P["user_items"][theuser][len(P_neg["user_items"][theuser][NUM_NEGATIVES:]):]
+        pos_scores = P["results"][theuser][len(P_neg["results"][theuser][NUM_NEGATIVES:]):]
+        for i, pos_item in enumerate(pos_items):
+            pos_score = pos_scores[i]
+            Zui[(theuser, pos_item)] = float(np.sum(all_scores > pos_score))
+    # calculate per-user scores
+    sum_user_auc = 0.0
+    sum_user_recall = 0.0
+    for theuser in P["users"]:
+        numerator_auc = 0.0
+        numerator_recall = 0.0
+        denominator = 0.0
+        for theitem in P["user_items"][theuser][len(P_neg["user_items"][theuser][NUM_NEGATIVES:]):]:
+            if theitem not in Ni:
+                continue
+            pui = np.power(Ni[theitem], (gamma + 1) / 2.0)
+            numerator_auc += (1 - Zui[(theuser, theitem)] / len(P["user_items"][theuser])) / pui
+            if Zui[(theuser, theitem)] < 1:
+                numerator_recall += 1.0 / pui
+            denominator += 1 / pui
+        if denominator > 0:
+            sum_user_auc += numerator_auc / denominator
+            sum_user_recall += numerator_recall / denominator
+    
+    return {
+        "auc"       : sum_user_auc / nonzero_user_count,
+        "recall"    : sum_user_recall / nonzero_user_count
+    }
+
+
+print(eq("wcml-yahoo-test-pos_evaluate_partial.pickle", "wcml-yahoo-test-neg_evaluate_partial.pickle", "training_arr.npy"))
+
